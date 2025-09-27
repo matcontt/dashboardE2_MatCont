c22c99d029593c8a8710ab74852741f74641e87c
alejandromateo186 <alejandromateo186@gmail.com>
2025-09-27 01:13:50 +0000

    ✨ feat(MoreDetails): agregar botones importados de IconButton - Se agregaron los botones y se alinearon al centro - Se busca crear un componente para editar estilos de botones

--- components/MoreDetails.tsx
+++ components/MoreDetails.tsx
@@ -2,11 +2,17 @@ import "@/global.css"
 import React from 'react'
 import { Image, View } from 'react-native'
 import CustomText from "./CustomText"
-
+import IconButton from "./IconButton"
 const MoreDetails = () => {
   return (
     <View className="w-screen h-1/2 bg-gray-800 bottom-0 left-0 right-0 rounded-t-xl p-5">
       <CustomText variant="subtitle2" value="Skills"/>
+      <View className="flex-row mt-4 gap-3 justify-center items-center">
+        <IconButton icon="logo-instagram" onPress={() => console.log("Instagram")} color="pink"></IconButton>
+        <IconButton icon="logo-whatsapp" onPress={() => console.log("WhatsApp")} color="green"></IconButton>
+        <IconButton icon="logo-facebook" onPress={() => console.log("Facebook")} color="blue"></IconButton>
+        <IconButton icon="logo-tiktok" onPress={() => console.log("Tiktok")} color="black"></IconButton>
+      </View>
     </View>
   )
 }
